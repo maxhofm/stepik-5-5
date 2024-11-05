c3036b4adda563e191d87cfe40c073df91306621
mhf <max.hofmann125x@gmail.com>
2024-11-05 15:49:35 +0300

    github link fix

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
 // this manifest is used temporarily for development purposes
-const manifestUrl = 'https://maxhofm.github.jo/stepik-5-5/tonconnect-manifest.json';
+const manifestUrl = 'https://maxhofm.github.io/stepik-5-5/tonconnect-manifest.json';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
